b821eac06cb3f84eec1ddd6d827651e387d367b5
Felix Baez <fbaez51@gmail.com>
2020-01-05 06:31:21 -0600

    Added logo and font.

--- ClientApp/src/components/NavMenu.js
+++ ClientApp/src/components/NavMenu.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import LoginButton from '../sub_components/LoginButton';
+import DadJokerLogo from '../images/DadJoker_Logo_400x400_transparent.png'
 import './NavMenu.css';
 
 export class NavMenu extends Component {
@@ -27,7 +28,7 @@ export class NavMenu extends Component {
             <header>
                 <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
                     <Container>
-                        <NavbarBrand tag={Link} to="/">Dad Joker</NavbarBrand>
+                        <NavbarBrand tag={Link} to="/"><font face="Alex Brush">Dad Joker</font><img src={DadJokerLogo}/> </NavbarBrand>
                         <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
                         <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
                             <ul className="navbar-nav flex-grow">
